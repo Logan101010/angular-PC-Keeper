f0145f3fc6ccf0471825919a15fb3e44765d3462
Logan101010 <angular1010@gmail.com>
2021-01-21 16:50:08 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-d5sjq1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-d5sjq1)
---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Calvins Team',
+    price: 799,
+    description: 'PC199'
+  },
+  {
+    id: 2,
+    name: 'Marks Team',
+    price: 699,
+    description: 'PC199'
+  },
+  {
+    id: 3,
+    name: 'Treys Team',
+    price: 299,
+    description: 'PC355'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
